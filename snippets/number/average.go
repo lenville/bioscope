116aa6b412d715249b112b11113d73b8d2e43105
lenville <lenville@gmail.com>
2015-01-25 14:22:34 +0800

    Update average.go & rename folder calculation to number

--- snippets/number/average.go
+++ snippets/number/average.go
@@ -4,6 +4,7 @@
  */
 
 package main
+
 import "fmt"
 
 func main() {
@@ -24,4 +25,21 @@ func main() {
 	}
 
 	fmt.Printf("Average: %f\n", avg)
+}
+
+/*
+ * 函数版本
+ */
+func average(xs []float64) (avg float64) {
+	sum := 0.0
+	switch len(xs) {
+	case 0:
+		avg = 0
+	default:
+		for _, v := range xs {
+			sum += v
+		}
+		avg = sum / float64(len(xs))
+	}
+	return
 }
