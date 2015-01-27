2d16e78baf8bddcdc2fba70a3531d1cf2cafe3ba
lenville <lenville@gmail.com>
2015-01-27 15:45:49 +0800

    Update map.go

--- snippets/tool/map.go
+++ snippets/tool/map.go
@@ -2,18 +2,21 @@ package main
 
 import "fmt"
 
+type e interface{}
+
 func mult2(f e) e {
 	switch f.(type) {
 	case int:
 		return f.(int) * 2
 	case string:
-		return f.(string) + f.(string) + f.(string) + f.(string)
+		// Double input string
+		return f.(string) + f.(string)
 	}
 	return f
 }
 
 func Map(n []e, f func(e) e) []e {
-	m := make([]e, len(n)
+	m := make([]e, len(n))
 	for k, v := range n {
 		m[k] = f(v)
 	}
@@ -21,5 +24,10 @@ func Map(n []e, f func(e) e) []e {
 }
 
 func main() {
-
+	m := []e{1, 2, 3, 4}
+	s := []e{"a", "b", "c", "d"}
+	mf := Map(m, mult2)
+	sf := Map(s, mult2)
+	fmt.Printf("%v\n", mf)
+	fmt.Printf("%v\n", sf)
 }
