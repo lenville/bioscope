057b3122b78fcf7ff66bf3c9e257928f9d65bf2b
lenville <lenville@gmail.com>
2015-02-03 14:50:40 +0800

    add bufio read testcase

---
+++ snippets/io/read-bufio.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"bufio"
+	"os"
+)
+
+func main() {
+	buf := make([]byte, 1024)
+	f, _ := os.Open("/etc/passwd")
+	defer f.Close()
+	r := bufio.NewReader(f)
+	w := bufio.NewWriter(os.Stdout)
+	defer w.Flush()
+	for {
+		n, _ := r.Read(buf)
+		if n == 0 {
+			break
+		}
+		w.Write(buf[0:n])
+	}
+}
